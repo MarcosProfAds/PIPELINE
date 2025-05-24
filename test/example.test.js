e8b546c2ca8f7e205e34d612f26d86688945c97b
unknown <professor@professor.com.br>
2025-05-24 10:32:34 -0300

    forcando o teste

--- test/example.test.js
+++ test/example.test.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 
 describe('Exemplo de Teste', function() {
   it('deve somar dois números corretamente', function() {
-    assert.strictEqual(1 + 12, 14);
+    assert.strictEqual(1 + 12, 15);
   });
 });
